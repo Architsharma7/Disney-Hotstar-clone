a966cd9406416297e0ab4e049b18bc7286851075
Archit Sharma <90101251+Architsharma7@users.noreply.github.com>
2022-08-02 23:55:35 +0530

    Update Login.js

--- disney-hotstar-clone/src/components/Login.js
+++ disney-hotstar-clone/src/components/Login.js
@@ -2,9 +2,21 @@ import styled from 'styled-components';
 import React from "react";
 
 const Login = (props) => {
-    return (
-    <div>Login</div>
+     return(
+     <Container>
+        <Content>
+          Content
+        </Content>
+     </Container>
     );
 };
+
+
+const Container = styled.section`
+   overflow: hidden;
+   display: flex;
+`;
+
+const Content = styled.div``;
 
 export default Login;
