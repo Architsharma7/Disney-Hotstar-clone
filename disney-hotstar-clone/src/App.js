3f0724efaa69aed1e1457691be7dbe2b15d4cbde
Archit <archit@Archits-MacBook-Air.local>
2022-08-02 23:32:47 +0530

    updated files

--- disney-hotstar-clone/src/App.js
+++ disney-hotstar-clone/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom" ; //advantage of react-router is that the page does not have to be refreshed when a link to another page is clicked
+import Login from "./components/Login";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className= "app">
+      <Router>
+        <Routes>
+          <Route exact path= "/" >
+            <Login />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ disney-hotstar-clone/src/components/Login.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import React from "react";
+
+const Login = (props) => {
+    return (
+    <div>Login</div>
+    );
+};
+
+export default Login;
