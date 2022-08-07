f571358e6c6edabc0604b0c20d42801b3bd420c6
Archit <archit@Archits-MacBook-Air.local>
2022-08-07 20:12:03 +0530

    updated files

--- disney-hotstar-clone/src/app/store.js
+++ disney-hotstar-clone/src/app/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit' ;
 import userReducer from "../features/users/userSlice";
+import movieReducer from "../features/movie/movieSlicer";
 
 export default configureStore({
     reducer: {
         user: userReducer,
+        movie: movieReducer,
     },
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
--- disney-hotstar-clone/src/components/Home.js
+++ disney-hotstar-clone/src/components/Home.js
@@ -1,11 +1,63 @@
 import styled from "styled-components";
 import React from "react";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
+import Recommends from "./Recommends";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Trending from "./Trending";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlicer";
+import { selectUserName } from "../features/users/userSlice";
 
 const Home = (props) => {
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trendings = [];
+
+    useEffect(() => {
+        db.collection("movies").onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                switch(doc.data().type){
+                    case "recommend" : 
+                        recommends = [...recommends, {id: doc.id, ...doc.data()}];
+                        break;
+                    case 'new':
+                        newDisneys = [...newDisneys, {id: doc.id, ...doc.data()}];
+                        break;
+                    case 'original':
+                        originals = [...originals, {id: doc.id, ...doc.data()}]
+                        break;
+                    case 'trending':
+                        trendings = [...trendings,{id: doc.id, ...doc.data()}]
+                        break;
+                }
+            });
+
+        dispatch(setMovies({
+            recommend: recommends,
+            newDisney: newDisneys,
+            original: originals,
+            trending: trendings,
+        })
+        );
+     });
+
+    }, [userName]);
+
     return (
         <Container>
             <ImgSlider/>
+            <Viewers/>
+            <Recommends/>
+            <NewDisney/>
+            <Originals/>
+            <Trending/>
         </Container>
     )
 };
