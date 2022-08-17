0886f28b419022a9b56a9aab16960d31dcdd6fa2
Archit <archit@Archits-MacBook-Air.local>
2022-08-18 03:14:22 +0530

    updated files

--- disney-hotstar-clone/src/components/Details.js
+++ disney-hotstar-clone/src/components/Details.js
@@ -1,21 +1,72 @@
 import styled from "styled-components";
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import db from "../firebase";
+import { collection, addDoc } from "firebase/firestore"; 
 
 const Detail = (props) => {
-    return <Container>
-        <Background>
-           <img alt="" src="" />
-        </Background>
+  const { id } = useParams();
+  const [detailData, setDetailData] = useState({});
+
+  useEffect(() => {
+    db.collection('movies')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists()) {
+          setDetailData(doc.data());
+        } else {
+          console.log("no such document in firebase");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }, [id]);
+
+  return (
+    <Container>
+      <Background>
+        <img alt="" src= {detailData.backgroundImg} />
+      </Background>
+      <ImageTitle>
+        <img alt="" src= {detailData.titleImg} />
+      </ImageTitle>
+      <ContentMeta>
+        <Controls>
+          <Player>
+            <img src="/images/play-icon-black.png" alt="" />
+            <span>Play</span>
+          </Player>
+          <Trailer>
+            <img src="/images/play-icon-white.png" alt="" />
+            <span>Trailer</span>
+          </Trailer>
+          <AddList>
+            <span />
+            <span />
+          </AddList>
+          <GroupWatch>
+            <div>
+              <img src="/images/group-icon.png" alt="" />
+            </div>
+          </GroupWatch>
+        </Controls>
+        <SubTitles>{detailData.subTitle}</SubTitles>
+        <Description>{detailData.description}</Description>
+      </ContentMeta>
     </Container>
+  );
 };
 
 const Container = styled.div`
-    position: relative;
-    min-height: calc(100vh -250px);
-    overflow-x: hidden;
-    display: block;
-    top: 72px;
-    padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  min-height: calc(100vh -250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
 `;
 
 const Background = styled.div`
@@ -31,6 +82,151 @@ const Background = styled.div`
     @media (max-width: 768px) {
       width: initial;
     }
+  }
+`;
+const ImageTitle = styled.div`
+  align-items: flex-end;
+  display: flex;
+  -webkit-box-pack: start;
+  justify-content: flex-start;
+  margin: 0 auto;
+  height: 30vw;
+  min-height: 170px;
+  padding-bottom: 24px;
+  width: 100%;
+
+  img {
+    max-width: 600px;
+    min-width: 200px;
+    width: 35vw;
+  }
+`;
+
+const ContentMeta = styled.div`
+  max-width: 874px;
+`;
+
+const Controls = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 24px 0px;
+  min-height: 56px;
+`;
+
+const Player = styled.button`
+  font-size: 15px;
+  margin: 0 22px 0 0;
+  padding: 0 24px;
+  height: 56px;
+  border-radius: 4px;
+  align-items: center;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  letter-spacing: 1.8px;
+  text-align: center;
+  text-transform: uppercase;
+  background: rgb(249, 249, 249);
+  border: none;
+  color: rgb(0, 0, 0);
+
+  img {
+    width: 32px;
+  }
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+
+  @media (max-width: 768px) {
+    height: 45px;
+    padding: 0px 12px;
+    font-size: 12px;
+    margin: 0 10px 0 0;
+
+    img {
+      width: 25px;
+    }
+  }
+`;
+
+const Trailer = styled(Player)`
+  background: rgb(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+
+const AddList = styled.div`
+  margin-right: 16px;
+  height: 44px;
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 50%;
+  border: 2px solid white;
+  cursor: pointer;
+
+  span {
+    background-color: rgb(249, 249, 249);
+    display: inline-block;
+
+    &:first-child {
+      height: 2px;
+      transform: translate(1px, 0px) rotate(0deg);
+      width: 16px;
+    }
+
+    &:nth-child(2) {
+      height: 16px;
+      transform: translate(-8px) rotate(0deg);
+      width: 2px;
+    }
+  }
+`;
+
+const GroupWatch = styled.div`
+  height: 44px;
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: white;
+  border-radius: 50%;
+  cursor: pointer;
+
+  div {
+    height: 40px;
+    width: 40px;
+    background-color: rgb(0, 0, 0);
+    border-radius: 50%;
+  }
+
+  img {
+    width: 100%;
+  }
+`;
+
+const SubTitles = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  padding: 16px 0px;
+  color: rgb(249, 249, 249);
+
+  @media (max-width: 768px) {
+    font-size: 14px;
   }
 `;
 
--- disney-hotstar-clone/src/features/users/userSlice.js
+++ disney-hotstar-clone/src/features/users/userSlice.js
@@ -25,8 +25,8 @@ const userSlice = createSlice({
 });
 
 export const { setUserLoginDetails, setSignoutState } = userSlice.actions;
-export const selectUserName = state => state.user.name;
-export const selectUserEmail = state => state.user.email;
-export const selectUserPhoto = state => state.user.photo;
+export const selectUserName = (state) => state.user.name;
+export const selectUserEmail = (state) => state.user.email;
+export const selectUserPhoto = (state) => state.user.photo;
 
 export default userSlice.reducer;
